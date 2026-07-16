1d0a4fbe42aa918630f69dd77501b79aab243e3e
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-07-15 14:40:03 -1000

    deps: refresh Hex dependencies

--- apps/synapse_web/mix.exs
+++ apps/synapse_web/mix.exs
@@ -41,7 +41,7 @@ defmodule SynapseWeb.MixProject do
       {:phoenix, "~> 1.8.5"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.2.7"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
